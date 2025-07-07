ad93bf3ba425b7ae966c9e47f09bbd4e8cbeb1ce
parkkkkjuneHyeon <gktkwk91@naver.com>
2025-07-07 16:30:25 +0900

    [feat]: search, catalog kafka연동 및 protobuf 도입

--- SearchService/SearchService/src/main/java/com/phiworks/works/SearchService/controller/SearchController.java
+++ SearchService/SearchService/src/main/java/com/phiworks/works/SearchService/controller/SearchController.java
@@ -1,11 +1,12 @@
 package com.phiworks.works.SearchService.controller;
 
-import com.phiworks.works.SearchService.dto.SearchRequestDTO;
 import com.phiworks.works.SearchService.service.SearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,21 +17,21 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    @PostMapping("/tags/register")
-    public ResponseEntity<Long> registerTag(
-            @RequestBody SearchRequestDTO searchRequestDTO) {
-        Long productId = searchService.addTagCache(searchRequestDTO);
-
-        return ResponseEntity.ok(productId);
-    }
-
-    @DeleteMapping("/tags")
-    public ResponseEntity<HttpStatus> deleteTag(
-            @RequestBody SearchRequestDTO searchRequestDTO) {
-        searchService.removeTagCache(searchRequestDTO);
-
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
+//    @PostMapping("/tags/register")
+//    public ResponseEntity<Long> registerTag(
+//            @RequestBody SearchRequestDTO searchRequestDTO) {
+//        Long productId = searchService.addTagCache(searchRequestDTO);
+//
+//        return ResponseEntity.ok(productId);
+//    }
+//
+//    @DeleteMapping("/tags")
+//    public ResponseEntity<HttpStatus> deleteTag(
+//            @RequestBody SearchRequestDTO searchRequestDTO) {
+//        searchService.removeTagCache(searchRequestDTO);
+//
+//        return ResponseEntity.ok(HttpStatus.OK);
+//    }
 
     @GetMapping("/tags/{tag}")
     public ResponseEntity<List<Long>> getTags(@PathVariable String tag) {
